564e09e1ba194d83cd9fe46d5ce6430712a6ed04
Erickowski <daniel_perez_03081995@hotmail.com>
2019-11-02 15:13:46 -0600

    diseño responsivo

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+    var botonSkill = document.getElementById("skills");
+    botonSkill.onclick = function(){
+        window.location="index.html";
+    };
+    
+  })();
